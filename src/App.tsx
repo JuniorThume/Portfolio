4d3fabcd4b55ac7486c536545fdffbaba19cae1a
Valmir Thume Junior <juninhothume21@gmail.com>
2025-01-14 09:37:33 -0300

    feat: Add go-top-button on the page

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import Navbar from './components/Navbar'
 import Presentation from './sections/Presentation'
 import SkillsAndTechs from './sections/SkillsTechs'
 import Contact from './sections/Contact'
+import GoTopButton from './components/GoTopButton'
 
 function App() {
 
   return (
-    <div>
+    <div className='relative'>
       <Navbar />
       <Presentation />
       <SkillsAndTechs />
-      <Contact/>
+      <Contact />
+      <GoTopButton />
     </div>
   )
 }
